b0fdfc723faa1e25d09d3346eef12e3d0d02aadd
yoid2000 <paul@francis.com>
2025-02-06 11:19:27 +0100

    added frequent words stuff

--- simple-word-match/app.py
+++ simple-word-match/app.py
@@ -8,8 +8,22 @@ wm = WordManager(num_words=10)
 def home():
     x = request.args.get('x')
     y = request.args.get('y')
-    print(f"x: {x}, y: {y}")
-    english_words, german_words = wm.get_words(english_index=int(x) if x is not None else None, german_index=int(y) if y is not None else None)
+    click_type = request.args.get('clickType')
+
+    # Extract and print the port information
+    host = request.host
+    if ':' in host:
+        hostname, port = host.split(':')
+    else:
+        hostname = host
+        port = '8100'  # Default port for HTTP if not specified
+    
+    print(f"Hostname: {hostname}")
+    print(f"Port: {port}")
+    wm.set_port(port)
+    
+    print(f"x: {x}, y: {y}, clickType: {click_type}")
+    english_words, german_words = wm.get_words(english_index=int(x) if x is not None else None, german_index=int(y) if y is not None else None, click_type=click_type)
     print(english_words)
     print(german_words)
     return render_template('index.html', english_words=english_words, german_words=german_words)
